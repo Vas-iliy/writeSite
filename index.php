0bb5375fd77d68a77d274764676c7517961e0a3c
Vasiliy <kkolyasev1228@gmail.com>
2020-05-19 16:05:37 +0300

    registration

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
+$connection = new PDO('mysql:host=localhost; dbname=write', 'root', 'root');
+
+function randomAuthKey () {
+    $char = '1234567890qwertyuiopasdfghjklzxcvbnmMNBVCXZASDFGHJKLPOIUYTREWQ';
+    $rand = '';
+    for ($i=0; $i < 20; $i++) {
+        $rand .= $char[rand(0, strlen($char)-1)];
+    }
+    return $rand;
+}
+
 $err = '';
 if ($_POST['name']) {
     $name = $_POST['name'];
@@ -7,11 +18,46 @@ if ($_POST['name']) {
     $email = $_POST['email'];
     $login = $_POST['login'];
     $password = $_POST['password'];
+    $authKey = randomAuthKey();
     if ($password != $_POST['password1']) {
         $err = 'Пароли не совпадают, повторите попытку';
     } else {
+        $data = $connection->query("INSERT INTO registrations 
+        (name, surname, country, email, login, password, authKey) VALUES 
+        ('$name', '$surname', '$country', '$email', '$login', '$password', '$authKey')");
 
+        if ($data) {
+            mail($email, 'Подтвердите почту', "http://writesite/?auth=$authKey");
+            echo 'Письмо отправлено. Подтвердите почту';
+        } else {
+            $findUser = $connection->query("SELECT * FROM registrations WHERE email = '$email'");
+            $findUser = $findUser->fetch();
+
+            if ($findUser['validation']) {
+                echo "<h2 style='color: green'>Вы уже зарегестрированы, войдите на сайт</h2>";
+                $name = '';
+                $surname = '';
+                $country = '';
+                $email = '';
+                $login = '';
+                $password = '';
+            } else {
+                echo "<h2 style='color: red'>Вы так и не подтвердили почту</h2>";
+                $name = '';
+                $surname = '';
+                $country = '';
+                $email = '';
+                $login = '';
+                $password = '';
+            }
+        }
     }
+}
+
+if ($_GET['auth']) {
+    $autg = $_GET['auth'];
+    $connection->query("UPDATE registration SET validation = true, newTime = current_timestamp ");
+    echo "<h2 style='color: blue'>Выша почта подтверждена, войдите на сайт</h2>";
 }
 
 ?>
