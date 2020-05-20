f234f8e7c99a9fe69dce4dac2a7baa720b9b66ad
Vasiliy <kkolyasev1228@gmail.com>
2020-05-20 14:58:11 +0300

    calls

--- article.php
+++ article.php
@@ -1,1 +1,19 @@
 <?php
+$connection = new PDO('mysql:host=localhost; dbname=write', 'root', 'root');
+$id = $_GET['id'];
+$state = $connection->query("SELECT states.title, states.content, states.newTime, registrations.name, 
+registrations.surname, registrations.country, registrations.login, cats.title 
+FROM states JOIN registrations USING (id_login) JOIN cats USING (id_cat)  WHERE states.id_state = '$id'");
+
+$images = $connection->query("SELECT images.id_img, images.title, images.extension 
+FROM state JOIN images USING (id_state)");
+
+$comments = $connection->query("SELECT comments.newTime, registrations.login, comments.comment 
+FROM states JOIN comments USING (id_comment) JOIN registrations USING (id_login) 
+WHERE states.id_state = '$id' AND comments.moder = 'yes' ORDER BY comments.newTime DESC ");
+
+?>
+
+<div>
+
+</div>
